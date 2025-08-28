ad13a4f81d7d67d1e313dd72b1320fd3c72a8ed6
Darhan-man <bolatovdarhan654@gmail.com>
2025-08-28 23:56:47 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-﻿from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
