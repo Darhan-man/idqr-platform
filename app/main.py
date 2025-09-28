98ebd080e9a8e8f6e0df61e7e79efa864fa2cf5e
Darhan-man <bolatovdarhan654@gmail.com>
2025-09-28 07:38:39 +0500

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ import textwrap
 import json
 import secrets
 from passlib.context import CryptContext
+from starlette.middleware.sessions import SessionMiddleware
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
